9b8f1747300c03602fbaabc38272487c2e761cae
Vitali Grauze <vgrauze@gmail.com>
2026-02-16 22:11:55 +0100

    Add Spring Boot project skeleton

---
+++ src/main/java/com/grauzev/pcremote/PcRemoteUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.grauzev.pcremote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcRemoteUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcRemoteUtilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grauzev/pcremote/PcRemoteUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grauzev.pcremote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcRemoteUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
