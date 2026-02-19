39f2b815e82ee53d8f764187fc2de2ac8dc3a78b
Vitali Grauze <vgrauze@gmail.com>
2026-02-19 22:33:45 +0100

    Remove temporary test error endpoint

--- src/main/java/com/grauzev/pcremote/api/StatusController.java
+++ src/main/java/com/grauzev/pcremote/api/StatusController.java
@@ -4,11 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class StatusController {
 	@GetMapping("/api/status")
 	public String status() {
 		return "ok";
 	}
-
+	
 }
