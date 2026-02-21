893775309872708fff2db39ee4b8e4a92b53669e
Vitali Grauze <vgrauze@gmail.com>
2026-02-21 22:32:01 +0100

    Finalize Command model

--- src/main/java/com/grauzev/pcremote/commands/Command.java
+++ src/main/java/com/grauzev/pcremote/commands/Command.java
@@ -2,4 +2,30 @@ package com.grauzev.pcremote.commands;
 
 public class Command {
 
+	private String id;
+	private String title;
+	private String type;
+	
+	public Command(String id, String title, String type) {
+		this.id = id;
+		this.title = title;
+		this.type = type;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	@Override
+	public String toString() {
+		return "Command{id='" + id + "', title='" + "', type='" + type + "'}";
+	}
 }
